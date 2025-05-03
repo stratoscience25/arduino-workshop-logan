8ba6aad6bf8ad778d48a1b05d041fd16c05899d2
bnobu <106547747+bnobu@users.noreply.github.com>
2025-05-03 16:17:33 -0400

    Completely redo code

--- LoRaRX/LoRaRX.ino
+++ LoRaRX/LoRaRX.ino
@@ -1,4 +1,3 @@
-//RX Credit to adafruit
 // Arduino9x_RX
 // -*- mode: C++ -*-
 // Example sketch showing how to create a simple messaging client (receiver)
@@ -9,12 +8,6 @@
 
 #include <SPI.h>
 #include <RH_RF95.h>
-#include <SoftwareSerialTX.h>
-#include <SD.h>
-
-const int chipSelect = 5;
-char *i;
-
 
 #define RFM95_CS 4
 #define RFM95_RST 2
@@ -25,30 +18,22 @@ char *i;
 
 // Singleton instance of the radio driver
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
-//SoftwareSerialTX ss(6);
-
 
 // Blinky on receipt
-//#define LED 13
+#define LED 13
 
-void setup() {
-  //ss.begin(9600);
-
-  // pinMode(LED, OUTPUT);
+void setup() 
+{
+  pinMode(LED, OUTPUT);     
   pinMode(RFM95_RST, OUTPUT);
   digitalWrite(RFM95_RST, HIGH);
+
+  while (!Serial);
   Serial.begin(9600);
   delay(100);
 
   Serial.println("Arduino LoRa RX Test!");
-  if (!SD.begin(chipSelect)) {
-    Serial.println("Card failed, or not present");
-    // don't do anything more:
-    while (1)
-      ;
-  }
-  Serial.println("card initialized.");
-
+  
   // manual reset
   digitalWrite(RFM95_RST, LOW);
   delay(10);
@@ -57,96 +42,52 @@ void setup() {
 
   while (!rf95.init()) {
     Serial.println("LoRa radio init failed");
-    while (1)
-      ;
+    while (1);
   }
   Serial.println("LoRa radio init OK!");
-
 
   // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
   if (!rf95.setFrequency(RF95_FREQ)) {
     Serial.println("setFrequency failed");
-    while (1)
-      ;
+    while (1);
   }
-  Serial.print("Set Freq to: ");
-  Serial.println(RF95_FREQ);
+  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
 
   // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
 
   // The default transmitter power is 13dBm, using PA_BOOST.
-  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
   // you can set transmitter powers from 5 to 23 dBm:
-  //rf95.setTxPower(23, false);
+  rf95.setTxPower(23, false);
 }
 
-void loop() {
-  // make a string for assembling the data to log:
-  String dataString = "";
-
-  if (rf95.available()) {
-    // Should be a message for us now
+void loop()
+{
+  if (rf95.available())
+  {
+    // Should be a message for us now   
     uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
     uint8_t len = sizeof(buf);
-    if (rf95.recv(buf, &len)) {
-
-      //digitalWrite(LED, HIGH);
+    
+    if (rf95.recv(buf, &len))
+    {
+      digitalWrite(LED, HIGH);
+      RH_RF95::printBuffer("Received: ", buf, len);
       Serial.print("Got: ");
-      Serial.println((char *)buf);
-      Serial.print("Id: ");
-      char *id = extractId(buf, "#");
-      Serial.println(id);
-      Serial.print("RSSI: ");
+      Serial.println((char*)buf);
+       Serial.print("RSSI: ");
       Serial.println(rf95.lastRssi(), DEC);
-
-      dataString = (String)id;
-
-      // for (i = id; *i != NULL; i++) {
-      //   ss.write(*i);
-      // }
-      // ss.write('\0');
-
-
-
-
-      //delay(1000);
-      //delay(10);
-
+      
       // Send a reply
-      uint8_t data[] = "KC1VVU-002";
+      uint8_t data[] = "And hello back to you";
       rf95.send(data, sizeof(data));
       rf95.waitPacketSent();
       Serial.println("Sent a reply");
-      //digitalWrite(LED, LOW);
-
-      // open the file. note that only one file can be open at a time,
-      // so you have to close this one before opening another.
-      File dataFile;
-      dataFile.close();
-      dataFile = SD.open("datalog.txt", FILE_WRITE);
-
-      // if the file is available, write to it:
-      if (dataFile) {
-        dataFile.println(dataString);
-        dataFile.close();
-        // print to the serial port too:
-        Serial.println(dataString);
-      }
-      // if the file isn't open, pop up an error:
-      else {
-        Serial.println("error opening datalog.txt");
-      }
-    } else {
+      digitalWrite(LED, LOW);
+    }
+    else
+    {
       Serial.println("Receive failed");
     }
   }
-}
-
-int extractId(char in[80], const char delim[2]) {
-  char *token;
-
-  token = strtok(in, delim);
-  token = strtok(NULL, delim);
-
-  return (token);
 }
